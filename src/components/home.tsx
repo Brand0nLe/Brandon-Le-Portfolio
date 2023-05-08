1a82b489d1f7e906abfe172e571ed3ca92b48b10
Brand0nLe <93brandonle@gmail.com>
2023-05-07 17:16:41 -0700

    updating dependencies and updated socialmedia urls

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -16,9 +16,9 @@ const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut turpis eget nunc efficitur blandit. Duis quis faucibus nulla. Sed id molestie justo. Proin non enim risus. Morbi non leo id justo pharetra congue vel ac orci.</p>
             <p>Ut nec nunc vitae ipsum tristique eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed ut ante nulla. Aenean viverra nisl ac dui porttitor commodo. Nam nec risus vel velit pretium commodo quis in lorem. Etiam a est sit amet nulla tincidunt bibendum.</p>
             <div className="social-icons">
-              <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-              <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-              <a href="mailto:email@example.com"><FaEnvelope /></a>
+              <a href="https://github.com/Brand0nLe" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+              <a href="https://www.linkedin.com/in/brandonctle/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+              <a href="mailto:brandonctle.dev@gmail.com"><FaEnvelope /></a>
             </div>
           </Col>
         </Row>
