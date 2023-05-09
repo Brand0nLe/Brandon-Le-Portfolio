c79aee5aa777e8b61986692f7ef021178181c85e
Brand0nLe <93brandonle@gmail.com>
2023-05-09 06:46:51 -0700

    Took out icons for socials in about me

--- src/components/pages/homepage/Home.tsx
+++ src/components/pages/homepage/Home.tsx
@@ -1,5 +1,145 @@
+// import { Container, Row, Col } from 'react-bootstrap';
+// import { FaGithub, FaLinkedin, FaEnvelope, FaFilePdf } from 'react-icons/fa';
+// import { motion } from 'framer-motion';
+// import { useState } from 'react';
+// import profilePicture from '../../assets/images/profilepicture.jpg';
+// import ResumeModal from '../../resumemodalcomponent/Resume';
+// import '../homepage/Home.css';
+
+// const Home = () => {
+//   const [modalOpen, setModalOpen] = useState(false);
+
+//   const handleOpenModal = () => {
+//     setModalOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   const profilePictureVariants = {
+//     initial: {
+//       border: "5px solid transparent",
+//       borderRadius: "50%",
+//       transition: { duration: 0.5 }
+//     },
+//     hover: {
+//       border: "5px solid #ffffff",
+//       borderRadius: "2%",
+//       transition: { duration: 0.5 }
+//     }
+//   };
+
+//   const socialIconsVariants = {
+//     initial: {
+//       scale: 1,
+//       opacity: 0,
+//       y: 0
+//     },
+//     animate: {
+//       scale: 2,
+//       opacity: 1,
+//       y: 0,
+//       transition: { delay: 1, duration: 2 }
+//     }
+//   };
+
+//   const iconVariants = {
+//     hover: {
+//       scale: 2,
+//       transition: { duration: 0.3 }
+//     },
+//     tap: {
+//       scale: 0.8,
+//       transition: { duration: 0.3 }
+//     }
+//   };
+
+//   return (
+//     <div id="home">
+//       <Container>
+//         <Row>
+//           <Col md={4}>
+//             <motion.img
+//               src={profilePicture}
+//               alt="Profile"
+//               className="img-fluid"
+//               variants={profilePictureVariants}
+//               initial="initial"
+//               whileHover="hover"
+//               onClick={handleOpenModal}
+//             />
+//           </Col>
+//           <Col md={6}>
+//             <Row className="align-items-center">
+//               <Col md={8}>
+//                 <h1 className="name-style mb-0">Brandon Le</h1>
+//               </Col>
+//               <Col md={4}>
+//                 <motion.div className="social-icons ml-auto" variants={socialIconsVariants} initial="initial" animate="animate">
+//                   <motion.a
+//                     href="https://github.com/Brand0nLe"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     variants={iconVariants}
+//                     whileHover="hover"
+//                     whileTap="tap"
+//                   >
+//                     <FaGithub />
+//                   </motion.a>
+//                   <motion.a
+//                     href="https://www.linkedin.com/in/brandonctle/"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     variants={iconVariants}
+//                     whileHover="hover"
+//                     whileTap="tap"
+//                   >
+//                     <FaLinkedin />
+//                   </motion.a>
+//                   <motion.a
+//                     href="mailto:brandonctle.dev@gmail.com"
+//                     variants={iconVariants}
+//                     whileHover="hover"
+//                     whileTap="tap"
+//                   >
+//                     <FaEnvelope />
+//                   </motion.a>
+//                   <motion.a
+//                     href="#"
+//                     onClick={handleOpenModal}
+//                     variants={iconVariants}
+//                     whileHover="hover"
+//                     whileTap="tap"
+//                   >
+//                     <FaFilePdf />
+//                   </motion.a>
+//                 </motion.div>
+//               </Col>
+//             </Row>
+
+
+//             <Row>
+//               <Col md={12}>
+//                 <p>
+//                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam veritatis voluptate adipisci ex nesciunt maiores aut inventore error consequatur sapiente cupiditate non labore nam mollitia quos omnis doloremque sint dolores, placeat fuga. Doloremque ex delectus aliquam impedit assumenda quasi deleniti tenetur nihil odit exercitationem ipsum odio, esse aspernatur quibusdam officia, saepe beatae libero, magnam nulla quaerat cumque aperiam neque culpa. Libero enim, atque reprehenderit excepturi eaque itaque, quo ipsa odio assumenda voluptate laudantium magnam incidunt architecto omnis mollitia soluta dignissimos rem corrupti? Dicta rerum vitae nam iure perferendis deleniti voluptas veritatis quis numquam voluptatibus officiis cumque, commodi dolores animi quisquam dolor soluta sint molestias! A, dolorum, voluptatibus omnis magni quidem itaque harum iste unde officiis, fugit eveniet! Rerum magni a ipsam mollitia sit. Porro esse ab commodi ut architecto dolor asperiores soluta fugiat sapiente nostrum rem dolore qui tenetur cumque facere adipisci eos pariatur voluptates, doloribus quasi, neque eaque modi. Excepturi atque placeat ratione expedita aliquid quis veniam ipsa consequatur eum error, dolor dicta perspiciatis officia fugiat quas illum autem, eligendi vero laborum a numquam nobis nihil aperiam! Beatae provident inventore, reiciendis veritatis, libero, quis omnis sed quos modi sapiente doloremque nulla quidem dolorum fuga ex culpa! Repudiandae, consequuntur fugit.
+//                 </p>
+//               </Col>
+//             </Row>
+
+//           </Col>
+//         </Row>
+//       </Container>
+//       {modalOpen && <ResumeModal closeModal={handleCloseModal} />}
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
 import { Container, Row, Col } from 'react-bootstrap';
-import { FaGithub, FaLinkedin, FaEnvelope, FaFilePdf } from 'react-icons/fa';
+import { FaFilePdf } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import profilePicture from '../../assets/images/profilepicture.jpg';
@@ -32,14 +172,14 @@ const Home = () => {
 
   const socialIconsVariants = {
     initial: {
-      scale: 8,
+      scale: 1,
       opacity: 0,
-      y: -50
+      y: 0
     },
     animate: {
-      scale: 5,
+      scale: 2,
       opacity: 1,
-      y: 100,
+      y: 0,
       transition: { delay: 1, duration: 2 }
     }
   };
@@ -70,51 +210,26 @@ const Home = () => {
               onClick={handleOpenModal}
             />
           </Col>
-          <Col md={8}>
-            <h1 className='name-style'>Brandon Le</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut turpis eget nunc efficitur blandit. Duis quis faucibus nulla. Sed id molestie justo. Proin non enim risus. Morbi non leo id justo pharetra congue vel ac orci.</p>
-            <p>Ut nec nunc vitae ipsum tristique eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed ut ante nulla. Aenean viverra nisl ac dui porttitor commodo. Nam nec risus vel velit pretium commodo quis in lorem. Etiam a est sit amet nulla tincidunt bibendum.</p>
-
-
-            <motion.div className="social-icons" variants={socialIconsVariants} initial="initial" animate="animate">
-              <motion.a
-                href="https://github.com/Brand0nLe"
-                target="_blank"
-                rel="noopener noreferrer"
-                variants={iconVariants}
-                whileHover="hover"
-                whileTap="tap"
-              >
-                <FaGithub />
-              </motion.a>
-              <motion.a
-                href="https://www.linkedin.com/in/brandonctle/"
-                target="_blank"
-                rel="noopener noreferrer"
-                variants={iconVariants}
-                whileHover="hover"
-                whileTap="tap"
-              >
-                <FaLinkedin />
-              </motion.a>
-              <motion.a
-                href="mailto:brandonctle.dev@gmail.com"
-                variants={iconVariants}
-                whileHover="hover"
-                whileTap="tap"
-              >
-                <FaEnvelope />
-              </motion.a>
-              <motion.a
-                href="#"
-                onClick={handleOpenModal}
-                variants={iconVariants}
-                whileHover="hover"
-                whileTap="tap"
-              >
-                <FaFilePdf />
-              </motion.a>
-            </motion.div>
+          <Col md={6}>
+            <Row className="align-items-center pl-5">
+              <Col md={8}>
+                <h1 className="name-style mb-0">Brandon Le</h1>
+              </Col>
+              {/* <Col md={4}>
+                <motion.div className="social-icons" variants={socialIconsVariants} initial="initial" animate="animate">
+                  <motion.a
+                    href="https://github.com/Brand0nLe"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variants={iconVariants}
+                    whileHover="hover"
+                    whileTap="tap"
+                  >
+                    <FaFilePdf />
+                  </motion.a>
+                </motion.div>
+              </Col> */}
+            </Row>
           </Col>
         </Row>
       </Container>
@@ -122,4 +237,5 @@ const Home = () => {
     </div>
   );
 };
+
 export default Home;
